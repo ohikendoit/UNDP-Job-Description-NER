9900bd4340a83a7eb8a6ef36f9e00c042abe1d63
ohikendoit <ohikendoit@gmail.com>
2021-07-05 09:02:39 +0300

    Progress commit

--- code/2. Training_NER_Model.py
+++ code/2. Training_NER_Model.py
@@ -42,35 +42,57 @@ def test_model(model, text):
         print(results)
     return (results)
 
-def train_spacy(data, iterations):
-    TRAIN_DATA = data
+def train_spacy(TRAIN_DATA, iterations):
     nlp = spacy.blank("en")
-    if "ner" not in nlp.pipe_names:
-        ner = nlp.create_pipe("ner")
-        nlp.add_pipe(ner, last=True)
-    for _, annotations in TRAIN_DATA:
-        for ent in annotations.get("entities"):
-            ner.add_label(ent[2])
-    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "ner"]
+    ner = nlp.create_pipe("ner")
+    ner.add_label("UNIT")
+    nlp.add_pipe(ner, name="unit_ner")
+
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "unit_ner"]
     with nlp.disable_pipes(*other_pipes):
         optimizer = nlp.begin_training()
         for itn in range(iterations):
-            print("Starting iteration" + str(itn))
+            print(f"Starting iteration {str(itn)}")
             random.shuffle(TRAIN_DATA)
             losses = {}
             for text, annotations in TRAIN_DATA:
-                nlp.update([text],
-                           [annotations],
-                           drop=0.2,
-                           sgd=optimizer,
-                           losses=losses)
-                print(losses)
-    return(nlp) #fully trained model
+                nlp.update( [text],
+                            [annotations],
+                            drop=0.2,
+                            sgd=optimizer,
+                            losses=losses,
+                )
+            print(losses)
+    return(nlp)
+
+#    if "ner" not in nlp.pipe_names:
+#        ner = nlp.create_pipe("ner")
+#        nlp.add_pipe(ner, last=True)
+#    for _, annotations in TRAIN_DATA:
+#        for ent in annotations.get("entities"):
+#            ner.add_label(ent[2])
+#    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "ner"]
+#    with nlp.disable_pipes(*other_pipes):
+#        optimizer = nlp.begin_training()
+#        for itn in range(iterations):
+#            print("Starting iteration" + str(itn))
+#            random.shuffle(TRAIN_DATA)
+#            losses = {}
+#            for text, annotations in TRAIN_DATA:
+#                nlp.update([text],
+#                           [annotations],
+#                           drop=0.2,
+#                           sgd=optimizer,
+#                           losses=losses)
+#                print(losses)
+#    return(nlp) #fully trained model
 
 
 nlp = spacy.load("unit_ner")
 
 TRAIN_DATA = load_data("../data/units_training_data.json")
+random.shuffle(TRAIN_DATA)
+
 nlp = train_spacy(TRAIN_DATA, 1)
 nlp.to_disk("ner_model")
-print(TRAIN_DATA)
+print(TRAIN_DATA[0:10])
